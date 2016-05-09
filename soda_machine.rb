d6bf0841d0541db24e24914535b941dce3d78394
Umar Farooq Bajwa <email": null>
2016-05-09 14:58:18 -0700

    Initial commit

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,36 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
+  @@current_inventory_count = 0
 
   def initialize(args = {})
-    @sodas = args[:sodas]
+    @sodas = Soda.new(@brand, @price)
     @cash = args[:cash]
+    @@current_inventory_count += 1
   end
 
-  def current_inventory_count
+  def self.current_inventory_count
+    @@current_inventory_count
   end
 
-  def find_soda(soda_brand)
-  end
+  # def find_soda(soda_brand)
+  #   return soda_brand if @sodas.has_value?(soda_brand)
+  # else
+  #   nil
+  # end
+  # end
 
-  def sell(soda_brand)
-  end
-
+  # def sell(soda_brand)
+  #   if find_soda(soda_brand) == true
+  #     @cash[soda_brand] = #cash +  && @sodas.delete[soda.brand]
+  #   else
+  #     nil
+  #   end
+  # end
 end
+
+pepsi = Soda.new(brand: "Pepsi", price: 0.65)
+p @@current_inventory_count
+mountain_dew = Soda.new(brand: "MD", price: 0.66)
+p @@current_inventory_count
